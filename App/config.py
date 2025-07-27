62bf443045453d04da20d66044e818c4b68b67a3
unknown <raliuga2001@gmail.com>
2025-07-26 19:18:28 -0500

    Fix: corregido el test.yml

--- App/config.py
+++ App/config.py
@@ -1,4 +1,9 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+env_file = ".env.test" if os.getenv("USE_TEST_ENV") == "1" else ".env"
+load_dotenv(dotenv_path=env_file)
 
 class Settings(BaseSettings):
     SECRET_KEY: str
