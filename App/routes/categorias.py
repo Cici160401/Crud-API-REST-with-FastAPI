087f2f6faadea8fd9210aa6da04b203dca6b0ef5
unknown <raliuga2001@gmail.com>
2025-07-31 11:37:47 -0500

    Actualizacion del delete, ya no tiene param response_model=Categoria

--- App/routes/categorias.py
+++ App/routes/categorias.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from database import get_db
+from fastapi import Response
 from typing import List, Optional
 from App.schemas.categorias import Categoria, CategoriaCreate, CategoriaUpdate
 from App.auth.dependencies import get_current_user
@@ -17,7 +18,7 @@ router = APIRouter (
     tags=["Categorias"]
 )
 
-#Buscar por nombre
+#Listar Categorias o Buscar por nombre
 @router.get("/", response_model= List[Categoria])
 def listar_categorias(
     search: Optional[str]= Query(None, description= "Buscar por nombre"),
@@ -41,12 +42,23 @@ def crear_nueva_categoria(categoria: CategoriaCreate, db: Session = Depends(get_
     return crear_categoria(db=db,categoria=categoria)
 
 #Eliminamos la categoria
-@router.delete("/{categoria_id}",response_model=Categoria)
-def borrar_categoria(categoria_id: int, db: Session = Depends(get_db), user : Usuario=Depends(get_current_user)):
+#@router.delete("/{categoria_id}",status_code=204)
+#def borrar_categoria(categoria_id: int, db: Session = Depends(get_db), user : Usuario=Depends(get_current_user)):
+#    categoria = eliminar_categoria(db, categoria_id)
+#    if not categoria :
+#        raise HTTPException(status_code=404,detail="Categoria no existe, no se puede eliminar")
+#    return Response(status_code=204)
+
+@router.delete("/{categoria_id}", status_code=200, response_model=dict)
+def borrar_categoria(
+    categoria_id: int,
+    db: Session = Depends(get_db),
+    user: Usuario = Depends(get_current_user)
+):
     categoria = eliminar_categoria(db, categoria_id)
-    if not categoria :
-        raise HTTPException(status_code=404,detail="Categoria no existe, no se puede eliminar")
-    return {"mensaje": "Categoria eliminada con éxito"}
+    if not categoria:
+        raise HTTPException(status_code=404, detail="Categoria no existe, no se puede eliminar")
+    return {"mensaje": "Categoría eliminada con éxito"}
 
 #Actualizamos mi categoría
 @router.put("/{categoria_id}", response_model=Categoria)
